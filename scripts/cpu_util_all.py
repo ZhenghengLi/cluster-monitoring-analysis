37a8c7c048eecfb5eda3bb6df2292b3836c76014
Zhengheng Li <zhenghenge@gmail.com>
2021-11-26 10:57:12 +0800

    cpu_util_all.py

--- scripts/cpu_util_all.py
+++ scripts/cpu_util_all.py
@@ -2,6 +2,7 @@
 
 import argparse
 import time
+from datetime import datetime
 import psycopg2
 
 parser = argparse.ArgumentParser(description='cpu usage statistics')
@@ -25,18 +26,25 @@ args = parser.parse_args()
 conn = psycopg2.connect(host='vm.physky.org', port=8862, dbname='cluster_monitoring',
                         user='cluster_monitoring_analysis', password='cluster_monitoring_password')
 
-args.begin_time *= 1000
-args.end_time *= 1000
+args.begin_time
+args.end_time
+timefmt = '%Y-%m-%dT%H:%M:%S'
 
 cur = conn.cursor()
 
 cur.execute("""
-select node, count(*)
+select count(*), min(time) / 1000, max(time) / 1000
 from node_cpu_load
-where time > %s and time < %s
-group by node
-""", (args.begin_time, args.end_time))
+where time > %s and time < %s and node=%s
+""", (args.begin_time * 1000, args.end_time * 1000, 'gpu01'))
 
-res = cur.fetchall()
+record_period_m, begin_time_s, end_time_s = cur.fetchone()
+actual_period_m = end_time_s / 60 - begin_time_s / 60
+begin_time = datetime.fromtimestamp(begin_time_s)
+end_time = datetime.fromtimestamp(end_time_s)
 
-print(res)
+print('query time range: %s => %s' %
+      (datetime.fromtimestamp(args.begin_time).strftime(timefmt), datetime.fromtimestamp(args.end_time).strftime(timefmt)))
+print('record period: %.2f days' % (record_period_m / 60.0 / 24.0))
+print('actual period: %.2f days (%s => %s)' %
+      (actual_period_m / 60.0 / 24.0, begin_time.strftime(timefmt), end_time.strftime(timefmt)))
