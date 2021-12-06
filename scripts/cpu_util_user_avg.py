92f04aa348e1ce400c48ae054ab5ea0bf855c510
Zhengheng Li <zhenghenge@gmail.com>
2021-12-06 09:40:58 +0800

    print busy ratio for each user

--- scripts/cpu_util_user_avg.py
+++ scripts/cpu_util_user_avg.py
@@ -48,17 +48,41 @@ print(' actual time range: %s => %s (%.2f days)' %
       (begin_time.strftime(timefmt), end_time.strftime(timefmt), actual_period_m / 60.0 / 24.0))
 
 cur.execute("""
-select "user",
-    round(cast(count(*) / 60.0 / 24.0 / 8.0 as NUMERIC), 2) as using_time_avg,
+select usage.user as "user",
+    round(usage.using_time_avg, 2) as using_time_avg,
+    round(cast(usage.cpu_time_avg as NUMERIC), 2) as cpu_time_avg,
     round(
-        cast(sum(cpu) / 100.0 / 60.0 / 24.0 / 8.0 as NUMERIC),
+        cast(coalesce(busy.busy_time_avg, 0) as NUMERIC),
         2
-    ) as cpu_time_avg,
-    round(cast(sum(cpu) / 100.0 / count(*) as NUMERIC), 2) as ratio_avg
-from user_cpu_mem
-where time > %(begin)s and time < %(end)s
-group by "user"
-having count(*) / 60.0 / 24.0 / 8.0 >= 0.0002
+    ) as fully_busy_avg,
+    round(
+        cast(
+            usage.cpu_time_avg / usage.using_time_avg as NUMERIC
+        ),
+        2
+    ) as cpu_ratio,
+    round(
+        cast(
+            coalesce(busy.busy_time_avg / usage.cpu_time_avg, 0) as NUMERIC
+        ),
+        2
+    ) as busy_ratio
+FROM (
+        select "user",
+            count(*) / 60.0 / 24.0 / 8.0 as using_time_avg,
+            sum(cpu) / 100.0 / 60.0 / 24.0 / 8.0 as cpu_time_avg
+        from user_cpu_mem
+        where time > %(begin)s and time < %(end)s
+        group by "user"
+        having count(*) / 60.0 / 24.0 / 8.0 >= 0.01
+    ) as usage
+    LEFT OUTER JOIN (
+        select "user",
+            sum(cpu) / 100.0 / 60.0 / 24.0 / 8.0 as busy_time_avg
+        from user_cpu_mem
+        where cpu > 95 and time > %(begin)s and time < %(end)s
+        group by "user"
+    ) as busy on usage.user = busy.user
 order by cpu_time_avg desc,
     using_time_avg desc
 """, {'begin': args.begin_time * 1000, 'end': args.end_time * 1000})
@@ -68,11 +92,12 @@ for desc in cur.description:
     name_list.append(desc[0])
 
 print()
-print('%10s %16s %15s %15s' % tuple(name_list))
-print('%10s %16s %15s %15s' % ('', '(days/node)', '(days/node)', ''))
+print('%10s %16s %15s %16s %15s %15s' % tuple(name_list))
+print('%10s %16s %15s %16s %15s %15s' %
+      ('', '(days/node)', '(days/node)', '(days/node)', '', ''))
 
 for record in cur:
-    print('%10s %16.2f %15.2f %15.2f' % record)
+    print('%10s %16.2f %15.2f %16.2f %15.2f %15.2f' % record)
 print()
 
 cur.close()
--- sql/cpu_utl_user_avg.sql
+++ sql/cpu_utl_user_avg.sql
@@ -1,12 +1,36 @@
-select "user",
-    round(cast(count(*) / 60.0 / 24.0 / 8.0 as NUMERIC), 2) as using_time_avg,
+select usage.user as "user",
+    round(usage.using_time_avg, 2) as using_time_avg,
+    round(cast(usage.cpu_time_avg as NUMERIC), 2) as cpu_time_avg,
     round(
-        cast(sum(cpu) / 100.0 / 60.0 / 24.0 / 8.0 as NUMERIC),
+        cast(coalesce(busy.busy_time_avg, 0) as NUMERIC),
         2
-    ) as cpu_time_avg,
-    round(cast(sum(cpu) / 100.0 / count(*) as NUMERIC), 2) as ratio_avg
-from user_cpu_mem
-group by "user"
-having count(*) / 60.0 / 24.0 / 8.0 >= 0.01
+    ) as busy_time_avg,
+    round(
+        cast(
+            usage.cpu_time_avg / usage.using_time_avg as NUMERIC
+        ),
+        2
+    ) as cpu_ratio,
+    round(
+        cast(
+            coalesce(busy.busy_time_avg / usage.cpu_time_avg, 0) as NUMERIC
+        ),
+        2
+    ) as busy_ratio
+FROM (
+        select "user",
+            count(*) / 60.0 / 24.0 / 8.0 as using_time_avg,
+            sum(cpu) / 100.0 / 60.0 / 24.0 / 8.0 as cpu_time_avg
+        from user_cpu_mem
+        group by "user"
+        having count(*) / 60.0 / 24.0 / 8.0 >= 0.01
+    ) as usage
+    LEFT OUTER JOIN (
+        select "user",
+            sum(cpu) / 100.0 / 60.0 / 24.0 / 8.0 as busy_time_avg
+        from user_cpu_mem
+        where cpu > 95
+        group by "user"
+    ) as busy on usage.user = busy.user
 order by cpu_time_avg desc,
     using_time_avg desc
