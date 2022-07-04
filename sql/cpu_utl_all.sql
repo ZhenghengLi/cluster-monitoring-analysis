f40e890c585def27398ea8918c625d6b7b126416
Zhengheng Li <zhenghenge@gmail.com>
2022-07-04 10:18:24 +0800

    update cpu_utl_all.sql

--- sql/cpu_utl_all.sql
+++ sql/cpu_utl_all.sql
@@ -11,30 +11,30 @@ select idle.node as node,
     round(cast(busy.cnt / usage.val as NUMERIC), 2) as busy_ratio
 from (
         select node,
-            count(*) as cnt
-        from node_cpu_load
-        where idle > 99.5
-        GROUP by node
-    ) as idle
-    INNER JOIN (
-        select node,
-            sum(1 - idle / 100.) as cnt
-        from node_cpu_load
-        where idle < 0.5
-        group by node
-    ) as busy on idle.node = busy.node
-    INNER JOIN (
-        select node,
-            sum(1 - idle / 100.) as val
-        from node_cpu_load
-        where idle <= 99.5
-        group by node
-    ) as usage on idle.node = usage.node
-    INNER JOIN (
-        select node,
             count(*) as cnt,
             min(time) as min_time,
             max(time) as max_time
         from node_cpu_load
         group by node
-    ) as total on idle.node = total.node
+    ) as total
+    LEFT OUTER JOIN (
+        select node,
+            count(*) as cnt
+        from node_cpu_load
+        where idle > 99.5
+        GROUP by node
+    ) as idle on total.node = idle.node
+    LEFT OUTER JOIN (
+        select node,
+            sum(1 - idle / 100.) as val
+        from node_cpu_load
+        where idle <= 99.5
+        group by node
+    ) as usage on total.node = usage.node
+    LEFT OUTER JOIN (
+        select node,
+            sum(1 - idle / 100.) as cnt
+        from node_cpu_load
+        where idle < 0.5
+        group by node
+    ) as busy on total.node = busy.node
