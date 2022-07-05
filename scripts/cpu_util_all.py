9cee5d276b0b7743199c513efe56e809cd4064bd
Zhengheng Li <zhenghenge@gmail.com>
2022-07-05 08:30:40 +0800

    cpu_util_all.py

--- scripts/cpu_util_all.py
+++ scripts/cpu_util_all.py
@@ -54,9 +54,9 @@ select total.node as node,
         2
     ) as total_time,
     round(total.cnt / 60.0 / 24, 2) as record_time,
-    round(idle.cnt / 60.0 / 24, 2) as fully_idle,
-    round((total.cnt - idle.cnt) / 60.0 / 24, 2) as using_time,
-    round(cast(usage.val / 60.0 / 24 as NUMERIC), 2) as cpu_time,
+    round(COALESCE(idle.cnt, 0) / 60.0 / 24, 2) as fully_idle,
+    round((total.cnt - COALESCE(idle.cnt, 0)) / 60.0 / 24, 2) as using_time,
+    round(cast(COALESCE(usage.val, 0) / 60.0 / 24 as NUMERIC), 2) as cpu_time,
     round(cast(COALESCE(busy.cnt, 0) / 60.0 / 24 as NUMERIC), 2) as fully_busy,
     round(cast(COALESCE(busy.cnt, 0) / usage.val as NUMERIC), 2) as busy_ratio
 from (
@@ -86,7 +86,7 @@ from (
         select node,
             sum(1 - idle / 100.) as val
         from node_cpu_load
-        where idle <= 99.5 and time > %(begin)s and time < %(end)s
+        where idle <= 99 and time > %(begin)s and time < %(end)s
         group by node
     ) as usage on total.node = usage.node
 """, {'begin': args.begin_time * 1000, 'end': args.end_time * 1000})
